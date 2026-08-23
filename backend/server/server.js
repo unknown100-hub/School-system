12e077b895c57a38dcb8e88b6fa90d657dc1c535
unknown100-hub <georgekamaugnk@email.com>
2026-08-23 23:49:24 +0300

    done

--- backend/server/server.js
+++ backend/server/server.js
@@ -28,7 +28,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 // Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
 });
 
